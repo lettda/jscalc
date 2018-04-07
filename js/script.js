f1fa60b31726b625bfc768f30b6027add32ffad4
lettda <lettda@vcu.edu>
2018-04-07 13:44:54 -0500

    calculations
    Display now shows value when button is pressed. Clear button now clears
    the display and '=' equals now logs the whole operation to execute

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 $(document).ready(() => {
-  console.log("Ready to run the app!");
-  $('button').click(() => {
-    if ($('button').attr('value') == 'clear') {
-      $('.calculationOutput').innerHTML = ' ';
-    } else {
-      $('.calculationOutput').append($('button').attr("value"));
-    }
-  });
 
+	$(".btn").click(function (event){
+		let clicked = this.innerHTML;
+		let calculationToExecute = $("#calculationHeader").html();
+		console.log(clicked);
+
+		if (clicked == "C") {
+			$("#calculationHeader").empty();
+		} else if (clicked == "=") {
+			console.log(typeof(calculationToExecute));
+			// $("#calculationHeader").html
+		} else {
+			$("#calculationHeader").append(clicked);
+		}
+	});
 
 });
